516947d1188dafbbc2315b5b71bb608d751bdb08
John Tyrone Pagunsan Coronel <jtcoronel.chmsu@gmail.com>
2026-02-14 12:14:18 +0800

    Learning Inheritance

---
+++ Inheritance/main.py
@@ -0,0 +1,4 @@
+# Inheritance = Allows a class  inherit attributes and method from another class
+#               Helps with code reusability and extensibility
+#               class Child(Parent)
+
